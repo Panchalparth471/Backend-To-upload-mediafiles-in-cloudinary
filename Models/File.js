f9b66a9678faf00be95828fddb6fef5ad9772353
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2023-12-06 11:56:14 +0530

    Update File.js

--- Models/File.js
+++ Models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 const File = mongoose.Schema({
     name: {
         type: String,
@@ -12,6 +14,37 @@ const File = mongoose.Schema({
     },
     email: {
         type:String
+    }
+})
+
+//post middleware
+File.post("save", async function (doc) {
+    try {
+        //Transporter for nodemailer
+        console.log("DOC ", doc);
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass:process.env.MAIL_PASS,
+            },
+        })
+
+        //send mail
+        let info = await transporter.sendMail({
+            from: 'Parth Panchal',
+            to: doc.email,
+            subject: "New file uploaded in cloudinary",
+            html: `<h1>Hello world</h1> <p>File Uploaded</p> View here <a href="${ doc.imageUrl }" >"${doc.imageUrl}"</a>`,
+        });
+       
+        console.log(info);
+    }
+    catch (e)
+    {
+        console.error(e);
+
+        
     }
 })
 
